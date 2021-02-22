06738da937ba16111a04fcceb5b98d296cfb249a
coderGriff66 <77123924+coderGriff66@users.noreply.github.com>
2021-02-22 17:35:14 -0500

    Added css, api Key & Urls, fixed searh button, h3

---
+++ app.js
@@ -0,0 +1,6 @@
+let apiKey = 71418fc0b590a4f9030dbe4361956ee6;
+let apiUrl = api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key};
+let apiUrl = api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key};
+
+
+
